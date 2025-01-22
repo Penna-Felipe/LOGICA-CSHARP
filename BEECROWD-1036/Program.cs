e44db4bc7a9306d93d22e4bbdd83973779605acd
Felipe <pennafelipe00@gmail.com>
2025-01-22 11:33:52 -0300

    ALTERACOES

--- BEECROWD-1036/Program.cs
+++ BEECROWD-1036/Program.cs
@@ -15,12 +15,12 @@ namespace BEECROWD_1036
             c = double.Parse(entrada[2]);
 
             delta = (Math.Pow(b, 2)) - (4 * a * c);
-            r1 = ((-b) + Math.Sqrt(delta)) / (2 * a);
-            r2 = ((-b) - Math.Sqrt(delta)) / (2 * a);
 
             if (delta < 0 || a <= 0 ){
                 Console.WriteLine("Impossivel calcular");
             } else {
+                r1 = ((-b) + Math.Sqrt(delta)) / (2 * a);
+                r2 = ((-b) - Math.Sqrt(delta)) / (2 * a);
                 Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
                 Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
             }
