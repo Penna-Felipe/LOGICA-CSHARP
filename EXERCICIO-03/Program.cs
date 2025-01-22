213162a743dc8305ca5b71696d79a9a3626038fc
Felipe <pennafelipe00@gmail.com>
2025-01-22 10:25:05 -0300

    AULA ESTRUTURA CONDICIONAL

---
+++ EXERCICIO-03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace EXERCICIO_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (nota1 + nota2) / 2.0;
+
+            if (media > 60.0){
+                Console.WriteLine("NOTA FINAL = " + media.ToString("F1", CultureInfo.InvariantCulture));
+            } else {
+                Console.WriteLine("NOTA FINAL = " + media.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
