4f1c0d7df378d903d0c7112394bd33860664c823
Felipe <pennafelipe00@gmail.com>
2025-01-22 11:59:40 -0300

    EXERCICIO-05

---
+++ EXERCICIO-05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+namespace EXERCICIO_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada;
+            double valor;
+
+            entrada = int.Parse(Console.ReadLine());
+            valor = 50.00;
+
+            if (entrada <= 100) {
+                Console.WriteLine("Valor a ser pago: R$" + valor.ToString("F2", CultureInfo.InvariantCulture));
+            } else {
+                valor += ((entrada - 100) * 2.00);
+                Console.WriteLine("Valor a ser pago: R$" + valor.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
