a20c2431be8b7436b0e2b2897550be6e93535027
Felipe <pennafelipe00@gmail.com>
2025-01-22 11:30:37 -0300

    EXERCICIO-04

---
+++ EXERCICIO-04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EXERCICIO_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            a = int.Parse(entrada[0]);
+            b = int.Parse(entrada[1]);
+            c = int.Parse(entrada[2]);
+
+            if (a >= b && a >= c){
+                Console.WriteLine("MAIOR = " + a);
+            } else 
+                if (b >= c) {
+                    Console.WriteLine("MAIOR = " + b);
+                } else {
+                        Console.WriteLine("MAIOR = " + c);
+                    } 
+            
+            if (a <= b && a <= c){
+                Console.WriteLine("MENOR = " + a);
+            } else 
+                if (b <= c) {
+                    Console.WriteLine("MENOR = " + b);
+                } else {
+                        Console.WriteLine("MENOR = " + c);
+                    } 
+        }
+    }
+}
